59630bc7a394279fd62a94b5cfe317602f2ae1be
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-09 03:31:00 +0600

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+var array = [1, 2, 3, 4, 5, 6];
+var result = array.splice(1, 2, 10, 12, 13, 19);
+console.log(result);
+// result will print the elements are are cut off: output is [ 2, 3 ]
+console.log(array);
+// array will be changed after using splice method.
+// output is : [
+//    1, 10, 12, 13,
+//    19,  4,  5,  6
+//  ]
